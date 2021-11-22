7bbd720a4caa897941b8efedbc5bc57a6e499759
dertrockx <iansalazar67@gmail.com>
2021-11-22 21:41:11 +0800

    task: added comments on index.js
    - added example code on how to use axios to query pokeapi.co

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-const { default: axios } = require("axios")
+const { default: axios } = require("axios");
 
 // TODO: async-await form of getPokemonTypes()
 const getPokemonTypesAsync = () => {
-  
-}
-
+  // Starter code on how to use axios to get types
+  // axios.get("https://pokeapi.co/api/v2/type/")
+  // NOTE: don't forget to use a try-catch block
+};
 
 // TODO: promise-then form of getPokemonTypes()
-const getPokemonTypesPromise = () => {
-
-}
-
+const getPokemonTypesPromise = () => {};
 
 // TODO: async-await form of getPokemonInfo()
 const getPokemonInfoAsync = () => {
-
-}
-
+  // Starter code on how to use axios to get pokemon
+  // axios.get("https://pokeapi.co/api/v2/pokemon/{id or name}")
+};
 
 // TODO: promise-then form of getPokemonInfo()
-const getPokemonInfoPromise = () => {
-
-}
+const getPokemonInfoPromise = () => {};
