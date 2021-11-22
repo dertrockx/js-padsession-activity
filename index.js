45348737ad3c0d5b687575409f503f10ebeaea8e
Arni Mendoza <acmendoza21@up.edu.ph>
2021-11-22 21:33:08 +0800

    Initial upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const { default: axios } = require("axios")
+
+// TODO: async-await form of getPokemonTypes()
+const getPokemonTypesAsync = () => {
+  
+}
+
+
+// TODO: promise-then form of getPokemonTypes()
+const getPokemonTypesPromise = () => {
+
+}
+
+
+// TODO: async-await form of getPokemonInfo()
+const getPokemonInfoAsync = () => {
+
+}
+
+
+// TODO: promise-then form of getPokemonInfo()
+const getPokemonInfoPromise = () => {
+
+}
