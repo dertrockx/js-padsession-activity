b252a6460c7f2f4823f6d4fca64f7bb56ec22c09
bsfelizardo <bsfelizardo@up.edu.ph>
2021-12-03 17:50:28 +0800

    MODIFIED: in file index.js: 	-	added implementation for getting pokemon types using 		async-await; 	-	added implementation for getting pokemon types using 		promise-then;

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const { default: axios } = require("axios");
 
 // TODO: async-await form of getPokemonTypes()
-const getPokemonTypesAsync = () => {
-  // Starter code on how to use axios to get types
-  // axios.get("https://pokeapi.co/api/v2/type/")
-  // NOTE: don't forget to use a try-catch block
+const getPokemonTypesAsync = async () => {
+  try{
+    const {data} = await axios.get("https://pokeapi.co/api/v2/type/");    // get data property from response
+    const pokemonData = data.results;                                     // get results property from data
+    console.log("\nPOKEMON TYPES USING ASYNC-AWAIT");
+    pokemonData.forEach((pokemonType) => {                                // loop through each result
+      console.log(` - ${pokemonType.name}`);                                      // print value of 'name' key in result
+    });
+  }
+  catch(error){                                                           // catch errors
+    console.log(error);                                                   // print error details
+  }
 };
 
 // TODO: promise-then form of getPokemonTypes()
-const getPokemonTypesPromise = () => {};
+const getPokemonTypesPromise = () => {
+  axios.get("https://pokeapi.co/api/v2/type/")
+	.then((res) => {
+		const pokemonData = res.data.results;                 // get results property from response
+		console.log("\n POKEMON TYPES USING PROMISE-THEN");
+    pokemonData.forEach((pokemonType) => {                // loop through each result
+      console.log(` - ${pokemonType.name}`);              // print value of 'name' key in result
+    });
+	})
+	.catch((err) => {                                       // catches the errors
+		console.log(err);                                     // print error details
+	});
+};
 
 // TODO: async-await form of getPokemonInfo()
 const getPokemonInfoAsync = () => {
@@ -18,3 +38,7 @@ const getPokemonInfoAsync = () => {
 
 // TODO: promise-then form of getPokemonInfo()
 const getPokemonInfoPromise = () => {};
+
+
+getPokemonTypesAsync();
+getPokemonTypesPromise();
