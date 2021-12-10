31d4413466220b99010fc0af2e9dfccfced249f6
seancua <sicua@up.edu.ph>
2021-12-11 02:59:30 +0800

    Modify: 	- In cua.js 		- TASK 1: async-await

--- cua.js
+++ cua.js
@@ -1,14 +1,28 @@
 const { default: axios } = require("axios");
 
 // TODO: async-await form of getPokemonTypes()
-const getPokemonTypesAsync = () => {
+const getPokemonTypesAsync = async () => {
   // Starter code on how to use axios to get types
   // axios.get("https://pokeapi.co/api/v2/type/")
   // NOTE: don't forget to use a try-catch block
+
+  try {
+    let types = await axios.get("https://pokeapi.co/api/v2/type/")
+    let get_data = types.data.results
+
+    for (let index in get_data) {
+      console.log(get_data[index].name)
+    }
+
+  } catch (err) {
+    console.log(err)
+  }
+
 };
 
 // TODO: promise-then form of getPokemonTypes()
 const getPokemonTypesPromise = () => {};
+
 
 // TODO: async-await form of getPokemonInfo()
 const getPokemonInfoAsync = () => {
@@ -18,3 +32,5 @@ const getPokemonInfoAsync = () => {
 
 // TODO: promise-then form of getPokemonInfo()
 const getPokemonInfoPromise = () => {};
+
+getPokemonTypesAsync();
