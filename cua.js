4b79f9cc9152b6680fb6575941ae79f1ced2b1a1
seancua <sicua@up.edu.ph>
2021-12-11 03:39:14 +0800

    Modify: 	- In cua.js 		- TASK 2: async-await 		- TASK 2: promise-then

--- cua.js
+++ cua.js
@@ -11,13 +11,13 @@ const getPokemonTypesAsync = async () => {
     const get_data = types.data.results
 
     console.log("----- TASK 1: async-await -----")
-    
+
     for (let index in get_data) {
       console.log(get_data[index].name)
     }
 
   } catch (err) {
-    console.log(err)
+    console.log("ERROR")
   }
 
 };
@@ -41,19 +41,56 @@ const getPokemonTypesPromise = () => {
     })
 
     .catch(err => {
-      console.log(err)
+      console.log("ERROR")
     })
 };
 
 
 // TODO: async-await form of getPokemonInfo()
-const getPokemonInfoAsync = () => {
+const getPokemonInfoAsync = async () => {
   // Starter code on how to use axios to get pokemon
   // axios.get("https://pokeapi.co/api/v2/pokemon/{id or name}")
+  try {
+    const pokemon = await axios.get("https://pokeapi.co/api/v2/pokemon/rayquaza")
+    const get_data = pokemon.data.moves
+
+    console.log("----- TASK 2: async-await -----")
+
+    for (let index in get_data) {
+      console.log(get_data[index].move.name)
+    }
+
+  } catch (err) {
+    console.log("ERROR")
+  }
+
 };
 
 // TODO: promise-then form of getPokemonInfo()
-const getPokemonInfoPromise = () => {};
+const getPokemonInfoPromise = () => {
+  const promise = new Promise(function (resolve, reject) {
+    resolve(axios.get("https://pokeapi.co/api/v2/pokemon/rayquaza"))
+  })
+
+  promise
+    .then(pokemon => {
+      const get_data = pokemon.data.moves
+
+      console.log("----- TASK 2: promise-then -----")
+
+      for (let index in get_data) {
+        console.log(get_data[index].move.name)
+      }
+
+    })
+
+    .catch(err => {
+      console.log("ERROR")
+    })
+
+};
 
 getPokemonTypesAsync();
 getPokemonTypesPromise();
+getPokemonInfoAsync();
+getPokemonInfoPromise();
